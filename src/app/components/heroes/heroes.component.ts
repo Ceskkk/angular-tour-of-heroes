238f77a274ac0982b2aab4e874cf75dae58e8e26
Cesk <ceskk97@gmail.com>
2022-12-24 12:58:27 +0100

    Created HeroService to share data, created Messages component and service

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../../models/hero';
 
-import { HEROES } from '../../mocks/heroes';
+import { Hero } from 'src/app/models/hero';
+import { HeroService } from 'src/app/services/hero.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-  
-  heroes: Hero[] = HEROES;
+
   selectedHero?: Hero;
 
-  constructor() { }
+  heroes: Hero[] = [];
 
-  ngOnInit(): void { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
